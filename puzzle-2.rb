b6b604711942c9240489b259141cb8701930d37c
Chris Gooley <cgooley@gmail.com>
2015-12-03 12:44:17 +1030

    cleaned puzzle 2

--- puzzle-2.rb
+++ puzzle-2.rb
@@ -3,12 +3,15 @@ total_ribbon = 0
 
 File.readlines('inputs/puzzle-2.txt').each do |p|
   dimensions = p.split('x').map(&:to_i)
+
+  # Calculate Wrapping
   wrapping = [2*dimensions[0]*dimensions[1], 2*dimensions[1]*dimensions[2], 2*dimensions[2]*dimensions[0]]
   smallest = wrapping.sort.first/2
+  total_wrapping += wrapping.inject(:+) + smallest
 
+  # Calculate Ribbon
   perimeter = dimensions.sort.first(2).map{|x| x*2}.inject(:+)
   total_ribbon += perimeter + dimensions.inject(:*)
-  total_wrapping += wrapping.inject(:+) + smallest
 end
 
 puts "Total Wrapping Paper: #{total_wrapping}"
