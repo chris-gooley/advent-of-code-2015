6829d6a7522e5381bfa69d7633f088c0034f3c40
Chris Gooley <cgooley@gmail.com>
2015-12-03 16:41:55 +1030

    cleaned up day3 puzzle

--- puzzle-3.part1.rb
+++ puzzle-3.part1.rb
@@ -1,21 +1,30 @@
+def getDirection(c)
+  case c
+  when '^'
+    [0,1]
+  when 'v'
+    [0,-1]
+  when '<'
+    [-1,0]
+  when '>'
+    [1,0]
+  else
+    [0,0]
+  end
+end
+
 houses_visited = [[0,0]]
 current_location = [0,0]
 
 File.open('inputs/puzzle-3.txt','r') do |maze|
   maze.each_char do |c|
-    current_location = case c
-    when '^'
-      [current_location[0],current_location[1]+1]
-    when 'v'
-      [current_location[0],current_location[1]-1]
-    when '<'
-      [current_location[0]-1,current_location[1]]
-    when '>'
-      [current_location[0]+1,current_location[1]]
-    else
-      break
-    end
+    direction = getDirection(c)
+
+    current_location = [current_location[0] + direction[0], current_location[1] + direction[1]]
+
     houses_visited << current_location if !houses_visited.include?(current_location)
   end
 end
 puts "Total Houses Visited: #{houses_visited.count}"
+
+
--- puzzle-3.part2.rb
+++ puzzle-3.part2.rb
@@ -1,3 +1,18 @@
+def getDirection(c)
+  case c
+  when '^'
+    [0,1]
+  when 'v'
+    [0,-1]
+  when '<'
+    [-1,0]
+  when '>'
+    [1,0]
+  else
+    [0,0]
+  end
+end
+
 houses_visited = [[0,0]]
 current_location = {
   santa: [0,0],
@@ -8,20 +23,11 @@ move = :santa
 
 File.open('inputs/puzzle-3.txt','r') do |maze|
   maze.each_char do |c|
-    cloc = current_location[move]
-    current_location[move] = case c
-    when '^'
-      [cloc[0],cloc[1]+1]
-    when 'v'
-      [cloc[0],cloc[1]-1]
-    when '<'
-      [cloc[0]-1,cloc[1]]
-    when '>'
-      [cloc[0]+1,cloc[1]]
-    else
-      break
-    end
-    houses_visited << cloc if !houses_visited.include?(cloc)
+    direction = getDirection(c)
+
+    current_location[move] = [current_location[move][0] + direction[0], current_location[move][1] + direction[1]]
+
+    houses_visited << current_location[move] if !houses_visited.include?(current_location[move])
 
     move = move == :santa ? :bot : :santa
   end
